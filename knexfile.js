6fb2a1175cf6224a947749c0756ce32a68d2426f
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-05 15:12:40 +0000

    update knexfile configure settings

---
+++ knexfile.js
@@ -0,0 +1,27 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/car-dealer.db3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/seeds'
+    }
+  },
+
+  production: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/car-dealer-production.db3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations'
+    }
+  }
+
+};
