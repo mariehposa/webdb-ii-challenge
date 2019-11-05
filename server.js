52f0e13ed63a7a67bfaf391995afda78aa804acf
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-05 21:21:47 +0000

    add endpoint to fetch all cars in database

--- server.js
+++ server.js
@@ -2,8 +2,19 @@ const express = require('express')
 const cors = require('cors')
 const helmet = require('helmet')
 
-const server = express()
+const db = require('./dbAccess/carsModel')
 
+const server = express()
+server.use(express.json())
+server.use(cors())
+server.use(helmet())
+
+
+server.get('/api/cars', (req, res) => {
+    db.get()
+    .then(data => res.status(200).json(data))
+    .catch(err => res.status(500).json({message: err.message}))
+})
 server.get('/', (req, res) => res.send('This works'))
 
 module.exports = server;
