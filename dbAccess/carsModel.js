1070b1ccf137df2848cfbf4397f60a7933e05beb
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-06 05:34:46 +0000

    mvp completed

--- dbAccess/carsModel.js
+++ dbAccess/carsModel.js
@@ -1,11 +1,20 @@
 const db =  require('../data/dbConfig')
 
 function get(id){
-    let query = db('cars as c')
+    let query = db('cars')
 
+    if (id) {
+        query.where('cars.id', id).first()
+    }
     return query;
 }
 
+function insert(car) {
+    return db('cars')
+    .insert(car)
+    .then(([ id ]) => this.get(id))
+}
 module.exports = {
-    get
+    get,
+    insert
 }
